f4cf95705a9dd432c7b8d915bf180acb65e1788e
Giorgi Kvariani <giokvarianiongithub@gmail.com>
2024-01-28 20:55:58 +0400

    Working on connect people

---
+++ BasePerson.Application/Features/Person/Commands/DisconnectPeopleCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasePerson.Application.Features.Person.Commands
+{
+    internal class DisconnectPeopleCommand
+    {
+    }
+}
